ba772d4cf1eb60f247856a98f82c070f73ceb21f
cwen0 <cwenyin0@gmail.com>
2017-06-30 10:35:25 +0800

    update

---
+++ importer/chepai.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+if  [ $# != 3 ];  then
+    echo "Usage: $0 port count tableName"
+    exit 1
+fi
+
+port=$1
+count=$2
+tableName=$3
+
+importer -t "CREATE TABLE ${tableName}(id bigint(21) NOT NULL AUTO_INCREMENT,name varchar(10) DEFAULT NULL,number varchar(7) DEFAULT NULL,city int(11) DEFAULT NULL,nation int(11) DEFAULT NULL,PRIMARY KEY (id),KEY cp (number))" \
+       -h  127.0.0.1  -P ${port} -D test  \
+       -n ${count}  \
+       -c 100 -b 100
