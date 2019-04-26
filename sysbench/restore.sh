b44829e006127958f87382bd89fe461cef39913f
cwen0 <cwenyin0@gmail.com>
2019-04-26 08:50:22 +0800

    add remove log

--- sysbench/restore.sh
+++ sysbench/restore.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 ansible-playbook stop.yml
+rm -rf /tmp/tidb/deploy/log/*
 ansible -i inventory.ini tikv_servers -m shell -a "cd /tmp/tidb/deploy/; rm -rf data; cp -R data.bak data"
 ansible -i inventory.ini pd_servers -m shell -a "cd /tmp/tidb/deploy/; rm -rf data.pd; cp -R datapd.bak data.pd"
 ansible-playbook start.yml
