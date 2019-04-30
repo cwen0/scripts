66ada974febc838bdfee69cac49a045c89969068
cwen0 <cwenyin0@gmail.com>
2019-04-30 20:47:10 +0800

    update update_data

--- sysbench/update_data.py
+++ sysbench/update_data.py
@@ -23,26 +23,56 @@ def handle_sysbench_file(path, name):
                 order by time desc limit 1 """ % (result["bench_method"]))
 
     last_result = dict()
-    if last_result is None or len(old_result) == 0:
-        last_result = None
-    else:
+    if last_result is not None and len(list(old_result.get_points())) != 0:
         last_result = list(old_result.get_points())[0]
+
+    if result["bench_time"] is None:
+        result["bench_time"] = datetime.datetime.now()
 
     data = [{
         "measurement": "benchbot",
         "tags": {
             "bench_type": result["bench_type"],
             "bench_method": result["bench_method"],
-            "cluster_info": result["cluster_info"]
+            "tidb_commit": result["cluster_info"]["tidb"]["commit"],
+            "tidb_branch": result["cluster_info"]["tidb"]["branch"],
+            "tidb_build_time": result["cluster_info"]["tidb"]["build_time"],
+            "tidb_tag": result["cluster_info"]["tidb"]["tag"],
+            "pd_commit": result["cluster_info"]["pd"]["commit"],
+            "pd_branch": result["cluster_info"]["pd"]["branch"],
+            "pd_tag": result["cluster_info"]["pd"]["tag"],
+            "pd_build_time": result["cluster_info"]["pd"]["build_time"],
+            "tikv_commit": result["cluster_info"]["tikv"]["commit"],
+            "tikv_branch": result["cluster_info"]["tikv"]["branch"],
+            "tikv_tag": result["cluster_info"]["tikv"]["tag"],
+            "tikv_build_time": result["cluster_info"]["tikv"]["build_time"]
         },
         "time": result.get("bench_time", datetime.datetime.now()),
-        "fields": result["bench_result"]}]
+        # "fields": result["bench_result"]}]
+        "fields": {
+            "tps_value": float(result.get("bench_result", {}).get("tps_value", 0)),
+            "tps_deviation": float(result.get("bench_result", {}).get("tps_deviation", 0)),
+            "tps_var": float(result.get("bench_result", {}).get("tps_var", 0)),
+            "tps_std": float(result.get("bench_result", {}).get("tps_std", 0)),
+            "qps_value": float(result.get("bench_result", {}).get("qps_value", 0)),
+            "qps_deviation": float(result.get("bench_result", {}).get("qps_deviation", 0)),
+            "qps_var": float(result.get("bench_result", {}).get("qps_var", 0)),
+            "qps_std": float(result.get("bench_result", {}).get("qps_std", 0)),
+            "lantency_avg_value": float(result.get("bench_result", {}).get("lantency_avg_value", 0)),
+            "lantency_avg_deviation": float(result.get("bench_result", {}).get("lantency_avg_deviation", 0)),
+            "lantency_avg_var": float(result.get("bench_result", {}).get("lantency_avg_var", 0)),
+            "lantency_avg_std": float(result.get("bench_result", {}).get("lantency_avg_std", 0)),
+            "lantency_95th_value": float(result.get("bench_result", {}).get("lantency_95th_value", 0)),
+            "lantency_95th_deviation": float(result.get("bench_result", {}).get("lantency_95th_deviation", 0)),
+            "lantency_95th_var": float(result.get("bench_result", {}).get("lantency_95th_var", 0)),
+            "lantency_95th_std": float(result.get("bench_result", {}).get("lantency_95th_std", 0)),
+            "time_elapsed": result.get("bench_result", {}).get("time_elapsed", 0)}}]
 
     try:
         client.write_points(data)
         # logging.info("writed %s to influxdb", result)
     except Exception as err:
-        logging.error("write %s to influxdb", result)
+        logging.error("write %s to influxdb", data)
         logging.traceback(err)
         sys.exit(1)
 
@@ -58,9 +88,11 @@ def main():
     files = os.listdir("data")
     for file in files:
         results.append(handle_sysbench_file("data", file))
-        # results[res["name"]] = res
 
-    # print(results[0].get("current"))
+    if len(results) <= 0:
+        logging.error("results is empty")
+        sys.exit(1)
+
     print("""Version:
 tidb: %s(%s) %s
 tikv: %s(%s) %s
@@ -74,6 +106,20 @@ pd: %s(%s) %s
            results[0].get("current", {}).get("cluster_info", {}).get("pd", {}).get("tag", ""),
            results[0].get("current", {}).get("cluster_info", {}).get("pd", {}).get("branch", ""),
            results[0].get("current", {}).get("cluster_info", {}).get("pd", {}).get("build_time", "")))
+
+    print("""Pre Version:
+tidb: %s(%s) %s
+tikv: %s(%s) %s
+pd: %s(%s) %s
+    """ % (results[0].get("last_point", {}).get("tidb_commit", ""),
+           results[0].get("last_point", {}).get("tidb_branch", ""),
+           results[0].get("last_point", {}).get("tidb_build_time", ""),
+           results[0].get("last_point", {}).get("tikv_branch", ""),
+           results[0].get("last_point", {}).get("tikv_build_time", ""),
+           results[0].get("last_point", {}).get("tikv_branch", ""),
+           results[0].get("last_point", {}).get("pd_tag", ""),
+           results[0].get("last_point", {}).get("pd_branch", ""),
+           results[0].get("last_point", {}).get("pd_build_time", "")))
 
     for index, value in enumerate(results):
         print("""test-%s: < %s >
