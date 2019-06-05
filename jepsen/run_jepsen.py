435c8aa0d0d6b0c19959770bb4cb7c0c7c69ea8a
cwen0 <cwenyin0@gmail.com>
2019-06-05 22:43:52 +0800

    jepen: update scripts

--- jepsen/run_jepsen.py
+++ jepsen/run_jepsen.py
@@ -93,7 +93,7 @@ def run_tests(offset, limit, unique_id, file_server):
 
 def update_stores(offset, limit, unique_id, file_server):
     end = offset+limit
-    store_name = "store-" + offset + "-" + end + ".tar.gz"
+    store_name = "store-" + str(offset) + "-" + str(end) + ".tar.gz"
     filepath = "tests/pingcap/jepsen/" + unique_id + "/" + store_name
     cmd = ["sh", "-c", "docker exec jepsen-control bash -c " +
            "'cd /jepsen/tidb/ && tar tar -zcvf " + store_name + " store && " +
